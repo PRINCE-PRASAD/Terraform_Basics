0a073fc72217ba3c2c3cde3512e5dc864986277d
PRINCE-PRASAD <princeprasad.code@gmail.com>
2023-06-23 20:40:36 +0530

    output by terraform

--- 17TERRAFORM-WITH-GITHUB/repositories.tf
+++ 17TERRAFORM-WITH-GITHUB/repositories.tf
@@ -5,13 +5,11 @@ resource "github_repository" "terraform-first-repository" {
   auto_init = true
 }
 
-
-resource "github_repository" "terraform-second-repository" {
-  name        = "second-repo-from-terraform"
-  description = "My second resource from terraform(Automated)"
-  visibility = "public"
-  auto_init = true
+output "repo-url" {
+  value = github_repository.terraform-first-repository.html_url
 }
+
+# terraform plan is use to see what changes will happen when we use terraform apply command
 # to check providers use command terraform providers
 # for download plugin use command terraform init (for install plug in of the providers)
 # create a toke from git hub account
@@ -19,4 +17,7 @@ resource "github_repository" "terraform-second-repository" {
 # for delete any ( first repo ) repo or resource use command terraform distroy --target github_repository.terraform-first-repository
 # for delete all repo or resource use command terraform destroy
 # for check the configuration is valid or not use command terraform validate
-#
+# for show thw tfstate file use command terraform show (use tio see all the resource config)
+# terraform refresh is command is use to check whether any changes done manully in github if happens where
+# always whatever written in terrafom file is final no manual changes will be entetained\
+# for check the output use the command terraform output repo-url (here repo-url is variable)
